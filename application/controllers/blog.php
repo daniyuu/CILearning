5e14e60485aec2145f5879f1f5ee4962f5818ed2
daniyuu <daniyuu@163.com>
2014-11-28 16:35:39 +0800

    hell world

---
+++ application/controllers/blog.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniyuu
+ * Date: 2014/11/28
+ * Time: 16:29
+ */
+
+class Blog extends CI_Controller{
+    function index(){
+        echo 'hello world';
+    }
+} 
