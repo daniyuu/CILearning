ea6b8cc5797163f510e634dfc5dffe284ea1956c
daniyuu <daniyuu@163.com>
2014-11-30 19:50:23 +0800

    add captche api and change build

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -8,14 +8,11 @@
 
 class Blog extends CI_Controller{
 
-    function Blog(){
-
-    }
-
     function index(){
         $data['title'] = "My blog title";
         $data['heading'] = "My blog heading";
         $data['todo'] = array("1","2","3","test");
-        $this->load->view('blog_view',$data);
+        //不需要写扩展名直接写目录名
+        $this->load->view('blog/index',$data);
     }
 } 
